9d04836297f29c92615550ae35f250e4bcff06ac
sheltons <shedzafyah2@gmail.com>
2023-07-12 00:17:35 +0200

    default class

---
+++ src/test/java/zw/co/sheltons/dronestask/DronesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.sheltons.dronestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
