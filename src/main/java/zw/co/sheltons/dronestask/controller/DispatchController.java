9173caef2d6343daabd6f7841b4d1ee971c91a34
sheltons <shedzafyah2@gmail.com>
2023-07-12 00:15:48 +0200

    adding battery level endpoint

--- src/main/java/zw/co/sheltons/dronestask/controller/DispatchController.java
+++ src/main/java/zw/co/sheltons/dronestask/controller/DispatchController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import zw.co.sheltons.dronestask.dto.DroneDTO;
 import zw.co.sheltons.dronestask.request.DroneRequest;
 import zw.co.sheltons.dronestask.service.DroneService;
+import zw.co.sheltons.dronestask.service.impl.BatteryLevelResponse;
 import zw.co.sheltons.dronestask.service.impl.DroneResponse;
 
 import java.util.List;
@@ -31,5 +32,11 @@ public class DispatchController {
         List<DroneDTO> responses = droneService.findAvailableDrones()
                 .stream().map(droneResponse -> DroneDTO.fromDTO(droneResponse.drone())).toList();
         return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{droneId}/battery")
+    public ResponseEntity<BatteryLevelResponse> checkBatteryLevel(@PathVariable Long droneId){
+        BatteryLevelResponse response = droneService.checkBatteryLevel(droneId);
+        return ResponseEntity.ok(response);
     }
 }
