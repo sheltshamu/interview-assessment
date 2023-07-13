b0a2505224e8b6747121bd1452fd9340ba9aeb0e
sheltons <shedzafyah2@gmail.com>
2023-07-13 20:57:27 +0200

    optimizing unused imports

--- src/main/java/zw/co/sheltons/dronestask/utils/Base64Deserializer.java
+++ src/main/java/zw/co/sheltons/dronestask/utils/Base64Deserializer.java
@@ -1,7 +1,6 @@
 package zw.co.sheltons.dronestask.utils;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
